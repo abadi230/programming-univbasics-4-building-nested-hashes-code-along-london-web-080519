91f1048c5823f756fcb6c4addf2ac6f1da874d59
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-07 21:45:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,8 +3,8 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        name: "",
-        age: {}
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
         #your key/value pairs here
@@ -17,8 +17,6 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        name: {},
-        age: {}
         },
       :matriarch => {
         #your key/value pairs here
